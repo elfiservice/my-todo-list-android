ea553a6e31bd628a94aae0c26bdb68eddf2a8ebb
Ant ARmando Junior <aaraujo@descartes.com>
2022-03-30 08:00:01 -0300

    feat : initiated components viewers

--- app/src/main/java/com/ajudanaweb/mytodolist/MainActivity.kt
+++ app/src/main/java/com/ajudanaweb/mytodolist/MainActivity.kt
@@ -2,10 +2,26 @@ package com.ajudanaweb.mytodolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var todoItem : EditText
+    lateinit var addButton : Button
+    lateinit var todoList : ListView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findAndInit()
+    }
+
+    private fun findAndInit() {
+        todoItem = findViewById(R.id.inputToDoItem)
+        addButton = findViewById(R.id.buttonAdd)
+        todoList = findViewById(R.id.todoList)
     }
 }
