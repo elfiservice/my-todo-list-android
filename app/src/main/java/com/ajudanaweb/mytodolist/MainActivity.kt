d08cbff141b4d5f2f29ca2d40a035b7e26eb975b
Ant ARmando Junior <aaraujo@descartes.com>
2022-03-31 08:12:12 -0300

    feat : provided the Adapter array to TodoList component

--- app/src/main/java/com/ajudanaweb/mytodolist/MainActivity.kt
+++ app/src/main/java/com/ajudanaweb/mytodolist/MainActivity.kt
@@ -2,28 +2,49 @@ package com.ajudanaweb.mytodolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var todoItem : EditText
-    lateinit var addButton : Button
-    lateinit var todoListView : ListView
+    private lateinit var todoItem : EditText
+    private lateinit var addButton : Button
+    private lateinit var todoListView : ListView
 
-    val todoList = ArrayList<String>()
+    private var todoList = ArrayList<String>()
+    private val fileHelper = FileHelper()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         initComponentsInScreen()
+        setupAdapter()
     }
 
     private fun initComponentsInScreen() {
         todoItem = findViewById(R.id.inputToDoItem)
         addButton = findViewById(R.id.buttonAdd)
         todoListView = findViewById(R.id.todoList)
+    }
+
+    private fun setupAdapter() {
+        todoList = fileHelper.readData(this)
+        val arrayAdapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            android.R.id.text1, todoList)
+
+        todoListView.adapter = arrayAdapter
+
+        addButton.setOnClickListener{
+            val todoName : String = todoItem.text.toString()
+            todoList.add(todoName)
+            todoItem.setText("")
+            fileHelper.writeData(todoList, applicationContext)
+            arrayAdapter.notifyDataSetChanged()
+        }
     }
 }
